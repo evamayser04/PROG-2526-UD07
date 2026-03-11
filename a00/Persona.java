d06c3229916d2c02dce3718fd52003d6c54725c8
Eva <evamayser04@gmail.com>
2026-03-11 16:50:54 +0100

    Modificacion

--- a00/Persona.java
+++ a00/Persona.java
@@ -1,8 +1,10 @@
+package a00;
+
 public class Persona {
-   protected int edad;
-   protected String nombre;
-   protected double estatura;
-   protected String nacionalidad;
+   private int edad;
+   private String nombre;
+   private double estatura;
+   private String nacionalidad;
 
     //constructor
     Persona (int edad, String nombre, double estatura String nacionalidad){
@@ -13,9 +15,44 @@ public class Persona {
     }
 
     public void mostrarDatos() {
-        System.out.println(nombre);
-        System.out.println(edad);
-        System.out.println(estatura);
-        System.out.println(nacionalidad);
+        System.out.println("\tNombre: " + getNombre());
+        System.out.println("\tEdad: " + getEdad() + " años");
+        System.out.println("\tEstatura: " + getEstatura() + " m");
+        System.out.println("\tNacionalidad: " + getNacionalidad());
     }
+
+    // Método público que trabaja con los atributos privados
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
+    }
+
+    // Getters y Setters protected: solo accesibles desde subclases
+
+    // Getters
+    protected int getEdad() {
+        return edad;
+    }
+    protected String getNombre() {
+        return nombre;
+    }        
+    protected double getEstatura() {
+        return estatura;
+    }
+     protected String getNacionalidad() {
+        return nacionalidad;
+    }
+    
+    // Setters
+    protected void setEdad(int edad) {
+        this.edad = edad;
+    }
+    protected void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    protected void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+    protected void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    } 
 }
