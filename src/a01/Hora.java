505e170267885e911f15e09c6b52532c3cdb63ea
Eva <evamayser04@gmail.com>
2026-03-13 14:08:58 +0100

    Modificacion

--- src/a01/Hora.java
+++ src/a01/Hora.java
@@ -1,5 +1,54 @@
 package a01;
 
 public class Hora {
+    private int h;
+    private int min;
 
+    Hora(int h, int min){
+        this.h = h;
+        this.min = min;
+    }
+
+    public void inc(){
+        min++;
+        if(min >= 60){
+            min = 0;
+            h++;
+        }if(h >= 24){
+            h = 0;
+        }
+    }
+
+    boolean setMinutos(int valor){
+        
+        boolean resultado = true;
+        
+        if(valor >= 60 ||valor < 0){
+            resultado =  false;
+        }else{
+            min = valor;
+        }
+        return resultado;
+    }
+
+    boolean setHora(int valor){
+        
+        boolean resultado = true;
+        
+        if(valor > 23 ||valor < 0){
+            resultado =  false;
+        }else{
+            h = valor;
+        }
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        String cad;
+        cad = "La hora es: " + h + ":" + min;
+        return cad;
+    }
+
+    
 }
--- src/a01/Main.java
+++ src/a01/Main.java
@@ -1,5 +1,21 @@
 package a01;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Dime la hora y minutos:");
+        Hora h = new Hora(sc.nextInt(), sc.nextInt());
+        System.out.println(h.toString());
+
+        System.out.println("Dime que minutos quieres que tenga");
+        h.setMinutos(sc.nextInt());
+
+        System.out.println("Dime que horas quieres que tenga");
+        h.setHora(sc.nextInt());
+
+        System.out.println(h.toString());
+    }
 }
